6956ec20648ffb2b2133d03d69d21a709a164164
Roch-Alexandre Nomine <roch@petaouchnok.org>
2019-08-07 15:53:14 -0400

    Fixed typo

--- wercker/prepare_image.sh
+++ wercker/prepare_image.sh
@@ -7,8 +7,8 @@ source $(dirname $0)/funcs.sh
 
 echo "Preparing image"
 cd $PHAROBUILD
-cp RoamingHub.image /var/pharo/images/61/HLR.image
-cp RoamingHub.changes /var/pharo/images/61/HLR.changes
+cp HLR.image /var/pharo/images/61/HLR.image
+cp HLR.changes /var/pharo/images/61/HLR.changes
 ln -s /usr/lib/i386-linux-gnu/libcrypto.so.1.0.0 /var/pharo/images/61/libcrypto.so
 
 cat > /startup.sh << __EOF__
